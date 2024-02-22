fe9eccbe9cba2b33293b8adc9c800e07077f4f92
Phil Zona <phil.zona@vercel.com>
2024-02-22 16:27:54 -0500

    Forgot to update an import

--- app/api/site-info/route.ts
+++ app/api/site-info/route.ts
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer-core';
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 
 export async function POST(req: Request, res: Response) {
   let errorMessage = 'Server error, please try again';
